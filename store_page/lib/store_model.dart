9ebe984274cbcf5f11879903df3cb517a0449e8f
Edirithanthiri Tharusha Kawshalya <tkedirithanthiri2005@gmail.com>
2025-02-13 12:01:46 +0530

    Update store_model.dart

--- store_page/lib/store_model.dart
+++ store_page/lib/store_model.dart
@@ -1,13 +1,20 @@
+// store_model.dart
 class Store {
   final String id;
   final String name;
   final String address;
-  final double distance; // Distance from the user's location
+  final double distance;
+  final String imageUrl;
+  final String openingHours;
+  final double rating;
 
   Store({
     required this.id,
     required this.name,
     required this.address,
     required this.distance,
+    required this.imageUrl,
+    required this.openingHours,
+    required this.rating,
   });
 }
