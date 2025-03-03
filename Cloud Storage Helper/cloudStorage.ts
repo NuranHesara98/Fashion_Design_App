ab667b3033f38f40614220a298e6542d58e11882
Pasandi Pabasara <165275465+pasandipabasara@users.noreply.github.com>
2025-03-03 20:30:43 +0530

    Add Cloud Storage Helper

--- Cloud Storage Helper/cloudStorage.ts
+++ Cloud Storage Helper/cloudStorage.ts
@@ -1,7 +1,11 @@
 import AWS from 'aws-sdk';
 import { S3 } from 'aws-sdk';
 
-const s3 = new S3();
+const s3 = new S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+});
 
 interface UploadFile {
     originalname: string;
