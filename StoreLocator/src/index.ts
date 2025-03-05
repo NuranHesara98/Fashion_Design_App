41d47a7f43e3aaae3e71cce17cd12e95d65a347f
Adeesha Vaas <adeesha.20222141@iit.ac.lk>
2025-03-06 00:08:37 +0530

    Update the database

--- StoreLocator/src/index.ts
+++ StoreLocator/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db';
 
 dotenv.config();
 
@@ -16,6 +17,9 @@ app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Welcome to Store Locator API' });
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+// Connect to MongoDB
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  });
 });
