f9f69881bdb895783b9398d993483bd9b425f253
Gayathri <sayatnethmini@gmail.com>
2025-03-08 20:40:54 +0530

    change onbording pages

--- clothing_app/lib/Onbording/onboarding_items.dart
+++ clothing_app/lib/Onbording/onboarding_items.dart
@@ -11,13 +11,13 @@ class OnboardingItems {
     OnboardingInfo(
       title: "Personalized Designs",
       description:
-          "You have complete control over your outfit's final look. Use our customization tools to adjust colors, patterns, and other details in your chosen design. Preview the results instantly and make adjustments until it's just right.",
+          "You have complete control over your outfit's final look. Use our customization tools to adjust colors, patterns, and other details in your chosen design.", 
       image: "assets/images/image2.jpeg",
     ),
     OnboardingInfo(
       title: "Seamless Experience",
       description:
-          "Once you're satisfied with your design, you can place your order directly through the app. Select a trusted tailor or shop from our list of verified vendors to produce your garment.So Enjoy a seamless experience from concept to creation!",
+          "Once you're satisfied with your design, you can place your order directly through the app. Select a trusted tailor or shop from our list of verified vendors to produce your garment.",
       image: "assets/images/image13.jpg",
     ),
   ];
