ef90874a90b9fdd2f4566db3bbc65ba58bd12af0
Nuran Chandrasekara <147916131+NuranHesara98@users.noreply.github.com>
2025-03-08 12:26:14 +0530

    Delete Filter Clothing Items directory

--- Filter Clothing Items/filterClothingItems.js
+++
@@ -1,9 +0,0 @@
-router.get('/clothing-items/filter', async (req, res) => {
-    try {
-        const category = req.query.category;
-        const items = await ClothingItem.find(category ? { category } : {});
-        res.json(items);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
