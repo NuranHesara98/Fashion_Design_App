275f403e0b49b698b99b3db3d7f4416bde556ac4
Edirithanthiri Tharusha Kawshalya <tkedirithanthiri2005@gmail.com>
2025-03-22 02:12:57 +0530

    not f

--- store_page/lib/main.dart
+++ store_page/lib/main.dart
@@ -22,6 +22,57 @@ class HomePage extends StatelessWidget {
 
   HomePage({required this.onThemeToggle});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.grey[900]!,
+              Colors.black,
+            ],
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'DRESS ME',
+                      style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                            color: Colors.white,
+                          ),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.favorite, color: Colors.white),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => FavoritesPage(),
+                              ),
+                            );
+                          },
+                        ),
+                       
+                    ),
+                  ],
+                ),
+              ),
+              // Rest of the HomePage code...
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
