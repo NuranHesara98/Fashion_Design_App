a2af4c3c0cf6fbafa5d7a07564114bcbe2850ccf
Nuran Chandrasekara <147916131+NuranHesara98@users.noreply.github.com>
2025-03-22 16:16:29 +0530

    Delete Login directory

--- Login/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts'],
-  setupFiles: ['<rootDir>/src/__tests__/setup.ts'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  }
-}; 
