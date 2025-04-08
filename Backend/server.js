bbf769bc5ec21763824c80363cf48b21a3eb8e9a
Nuran Chandrasekara <nuranhesara321@gmail.com>
2025-04-08 15:59:08 +0530

    update code

---
+++ Backend/server.js
@@ -0,0 +1,5 @@
+// This file is a simple entry point for Render.com deployment
+// It redirects to the actual server.js file in the src directory
+
+console.log('Starting server from root server.js...');
+require('./src/server.js');
